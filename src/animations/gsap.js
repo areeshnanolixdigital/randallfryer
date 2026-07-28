1d045d28f9702a76d2bad64c918314495803d64a
Areesh Raza Khan <99598364+AreeshRazaKhan@users.noreply.github.com>
2026-07-28 20:22:27 +0500

    feat(playwright): add utility for context options with headers and implement screenshot functionality

--- src/animations/gsap.js
+++ src/animations/gsap.js
@@ -1,7 +1,7 @@
 "use client";
 
-import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
